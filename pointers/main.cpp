e4f5a5b8a1170b185d1edcc23f67aed87cc51332
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-06 19:52:49 +0100

    poinet

---
+++ pointers/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+
+int main () {
+
+
+int num{14};
+
+cout << " the value held at num is: " << num << endl;
+
+cout << " the size of the value held in bytes is : " << sizeof(num) << endl;
+
+
+cout << " the address if the integer num is : " << &num << endl;
+
+/////////////// now for pointers //////////
+
+
+    return 0;
+}
--- selectionSort/main.cpp
+++ selectionSort/main.cpp
@@ -1,22 +1,59 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
+void selectionSort(vector<int> arr, int n);
+void swap(vector<int> arr, int i, int j);
 
-int main () {
+int main()
+{
 
+    vector<int> arr{3, 5, 3, 2, 7, 8, 9, 1};
 
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+    }
 
+    cout << "\n \n ............... \n sorted \n ......... \n";
 
-cout << " sorting select " ;
+    selectionSort(arr, n);
 
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+    }
 
-
-return 0;
+    return 0;
 }
 
+void selectionSort(vector<int> arr, int n)
+{
 
+    for (int i = 0; i < n; i++)
+    {
 
+        int min = i;
+
+        for (int j = i; j < n; j++)
+        {
+
+            if (arr[j] < arr[min])
+
+                min = j;
+        }
+
+        swap(arr, min, i);
+    }
+}
+
+void swap(vector<int> arr, int i, int j)
+{
+
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
