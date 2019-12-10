412790eb6600ae7230bc49f9d97942de4fc6e22c
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-10 13:37:20 +0100

    update to functions

--- quickSort/main.cpp
+++ quickSort/main.cpp
@@ -8,6 +8,10 @@ using namespace std;
 
 int quickSort( int arr[], int low, int high);
 
+int partition( int arr[], int low, int high);
+
+void swap (int *a, int *b);
+
 
 
 int main () {
@@ -15,25 +19,78 @@ int main () {
 
 int arr[] { 1,32,12,45,33,22,66,77,88,11,7};
 int low = 0;
-
+int size = sizeof(arr) / sizeof(arr[0]);
 size_t  high = (sizeof(arr) / sizeof(arr[0])) -1;
+
+cout << "                                         \n \n \n \n \n  ";
+for (int i = 0; i < size; i++) {
+
+cout << arr[i];
+
+}
 
 quickSort( arr, low, high);
 
 
+cout << "                                         \n \n \n \n \n  ";
 
+for (int i = 0; i < size; i++) {
 
+cout << arr[i];
 
+}
     return 0;
 }
 
 
+//////////////////////////////////// QUICK SORT ================================
+
+
 int quickSort (int arr[],int low, int high) {
 
-if low
+if (low < high) {
+
+int pivotPartitioned = partition(arr,low,high);
 
 
+quickSort(arr, low, pivotPartitioned -1);
+quickSort(arr,pivotPartitioned+1, high);
 
+}
 
    return 0; 
+}
+
+
+//////////////////////////////////// PARTITION ================================
+
+int partition (int arr[], int low, int high) {
+
+    int pivot = arr[high];
+
+    int i = (low -1);
+
+for  ( int j = low; j <= high -1; j++){
+
+        if (arr[j] < pivot) {
+
+            i++;
+            swap(&arr[i], &arr[j]);
+
+        }
+}
+    swap(&arr[i+1], &arr[high]);
+
+    return (i+1);
+}
+
+//////////////////////////////////// SWAP ================================
+
+void swap (int *a, int *b) {
+
+    int temp = *a;
+
+    *a = *b;
+   *b = temp;
+
 }
