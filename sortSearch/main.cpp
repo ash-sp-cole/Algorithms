d011bb7d9ed9281df0a12fc2f5613eb5b3e7e270
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-11 17:09:30 +0100

    sort working

--- sortSearch/main.cpp
+++ sortSearch/main.cpp
@@ -15,7 +15,7 @@ int main()
 
     // set up
 
-    int arr[]{3, 5, 2, 7, 9, 1, 6, 11, 55, 22, 77, 777, 44, 88, 255, 0, 33, 65, 23, 86, 14};
+    int arr[]{3, 5, 2, 7, 9, 1, 6, 11, 864, 55, 22, 77, 777, 44, 88, 255, 0, 33, 65, 23, 86, 14};
 
     int low{};
 
@@ -41,40 +41,37 @@ int main()
 
 int partition(int arr[], int low, int high)
 {
-    
-    int pIndex = (low-1);
+
+    int pIndex = (low - 1);
     int pivot = arr[high];
 
-    for (int j = low; j < high; j++) {
+    for (int j = low; j < high; j++)
+    {
 
-            if (arr[j] < pivot) {
+        if (arr[j] < pivot)
+        {
 
-                pIndex++;
-                swap(&arr[pIndex], &arr[j]);
-
-            }
+            pIndex++;
+            swap(&arr[pIndex], &arr[j]);
+        }
     }
 
-    swap(&arr[pIndex+1],&arr[high]);
-  
-  return (pIndex+1);
+    swap(&arr[pIndex + 1], &arr[high]);
+
+    return (pIndex + 1);
 }
 
 void quickSort(int arr[], int low, int high)
 {
 
+    if (low < high)
+    {
 
-        if (low < high) {
+        int pi = partition(arr, low, high);
 
-    int pi = partition(arr,low,high);
-
-
-            quickSort(arr, low , pi-1);
-            quickSort(arr,pi +1, high);
-
-        }
-
-    
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
 void swap(int *a, int *b)
