0f1864bdb5478d134c748b865c7be0271def3725
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-18 18:02:16 +0100

    update

--- sortSearch/main.cpp
+++ sortSearch/main.cpp
@@ -1,141 +1,97 @@
 #include <iostream>
 
-// quick sort
+int partition(int arr[], int low, int high);
 int quickSort(int arr[], int low, int high);
 
-int partition(int arr[], int low, int high);
-
-// swap
 void swap(int *a, int *b);
 
-// binary
-int binarySearch(int arr[], int n, int targetValue);
-
-// print
 void print(int arr[], int n);
 
-
+int binarySearch(int arr[], int n);
 
 int main()
 {
 
-    // setup ===================================================
-    int targetValue{};
+    int arr[]{1, 3, 55, 33, 66, 88, 2242, 7474, 131, 386, 42, 4, 2, 3, 4, 5, 6, 4, 3, 2};
 
-    std::cout<< " enter target value :   \n";
-    std::cin >> targetValue;
-    int arr[]{1, 4, 2, 5, 1, 6, 7, 2, 44, 11, 55, 33, 77, 55, 88, 99, 66, 133, 432, 564, 768, 14, 9};
-
-    int low = {0};
+    int low{};
 
     size_t n = sizeof(arr) / sizeof(arr[0]);
 
-    int high = n - 1;
+    int high = (n - 1);
 
-    std::cout << " \n \n \n \n";
+    std::cout << " \n \n \n unsorted array: \n \n";
 
-    print(arr, n);
+    print(arr,n);
 
-    std::cout << " \n \n \n \n";
+    quickSort(arr,0,high);
 
-    quickSort(arr, 0, high);
+    std::cout << " \n \n \n sorted array: \n \n";
 
-
-    print(arr, n);
-
-    binarySearch( arr,  n,  targetValue);
-    return 0;
-}
-
-
-
-
-
-// ================================================================================================
-
-
-int binarySearch(int arr[], int n, int targetValue) {
-
-
-    int low {0};
-    int high = n-1;
-    int mid{};
-
-    
-    while ( low <= high )
- {
-        mid = (low +high) / 2;
-     if (targetValue == arr[mid]) {
-
-         std::cout <<" \n \n target found " << targetValue << std::endl;
-         std::cout << " at index : " << mid;
-            break;
-     }
-
-    else if( targetValue < arr[mid]) {
-
-            high = mid-1;
-
-    }     
-    else ( low = mid +1);
- }
-        return 0;
- }
-
-
-// quick sort ==================
-
-int quickSort(int arr[], int low, int high)
-{
-
-    if (low < high)
-    {
-
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+    print(arr,n);
 
     return 0;
 }
 
-int partition(int arr[], int low, int high)
-{
 
-    int pIndex = (low - 1);
+int quickSort(int arr[], int low, int high) {
+
+if (low <  high ) {
+std::cout << " \n if loop quick \n";
+    int pi = partition(arr, low, high);
+
+    quickSort(arr, low, pi-1);
+
+    quickSort(arr, pi+1, high);
+}
+return 0;
+
+}
+
+
+int partition (int arr[], int low, int high) {
+std::cout << " \n part \n";
+    int pIndex = (low -1);
 
     int pivot = arr[high];
 
-    for (int j = low; j < high; j++)
-    {
 
-        if (arr[j] < pivot)
-        {
-            
-            pIndex++;
-               swap(&arr[pIndex], &arr[j]);
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] < pivot) {
+
+                pIndex++;
+
+                swap(&arr[j],&arr[pIndex]);
+
+            }
         }
-    }
-    swap(&arr[pIndex + 1], &arr[high]);
 
-    return (pIndex + 1);
+        swap (&arr[pIndex+1], &arr[high]);
+
+return (pIndex+1);
+
 }
 
-void swap(int *a, int *b)
-{
 
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap (int *a, int *b) {
+std::cout << " \n swap \n";
+int temp = *a;
+
+*a =*b;
+
+*b = temp;
+
 }
 
-void print(int arr[], int n)
-{
 
-    for (int i = 0; i < n; i++)
-    {
+void print (int arr[], int n) {
 
-        std::cout << "   " << arr[i];
+    for (int i =0; i < n; i++) {
+
+
+        std::cout << "  " << arr[i];
+
+
     }
 }
