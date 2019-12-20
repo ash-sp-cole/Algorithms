c4c1a669029a3ae87e8482195f77f673e0b19f64
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-20 15:15:46 +0100

    update

--- testSpace/main.cpp
+++ testSpace/main.cpp
@@ -9,28 +9,27 @@ using namespace std;
 int main()
 {
 
-int arr[]{1,2,3,4,5,6,7,8,9,10,11,12,13,15,16};
+int arr[]{1,2,3,4,5,6,7,8,10,11,12,13,14,15};
 
 
 size_t n = sizeof(arr) / sizeof(arr[0]);
 
+int total = (n+1) * (n+2) / 2;
 
-int total = (n +1) * (n +2) / 2;
+for  (int i = 0; i < n; i++) {
 
+    total -= arr[i];
 
-
-for (int i =0; i < n; i++) {
-
-
-  total -=arr[i];
 
 
 
 }
 
-
-
 cout << total;
+
+
+
+
     return 0;
 }
 
