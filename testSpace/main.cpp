c1edcda3fc1de96074e3de5645d1773e0b9d7544
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-20 14:09:42 +0100

    using sum to find missing arr value

--- testSpace/main.cpp
+++ testSpace/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
-
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
@@ -9,7 +9,28 @@ using namespace std;
 int main()
 {
 
+int arr[]{1,2,3,4,5,6,7,8,9,10,11,12,13,15,16};
 
+
+size_t n = sizeof(arr) / sizeof(arr[0]);
+
+
+int total = (n +1) * (n +2) / 2;
+
+
+
+for (int i =0; i < n; i++) {
+
+
+  total -=arr[i];
+
+
+
+}
+
+
+
+cout << total;
     return 0;
 }
 
