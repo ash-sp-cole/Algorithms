499edfa106c28a8077a5dd4f0893c000150e3bc6
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-26 00:55:02 +0100

    refactor

--- testSpace/main.cpp
+++ testSpace/main.cpp
@@ -1,55 +1,57 @@
 #include <iostream>
 
-int main()
-{
-
-    int arr[]{1, 2, 2, 3, 3,4, 5, 6, 4, 6, 4, 4, 4, 7};
-
-    size_t n = sizeof(arr) / sizeof(arr[0]);
-
-    std::cout << " size of array is " << n << std::endl;
-
-    for (int i = 0; i < n; i++)
-    {
-
-        std::cout << arr[i] << "  ";
-    }
-
-    std::cout << " sorting array for possible duplicates" << std::endl;
-
-    for (int i = 0; i < n; i++)
-       for (int k = i+1; k < n;)
-        {
-
-            if (arr[i] == arr[k])
-            {
-
-               
-                    for (int j = k; j < n-1; j++){
-                        
-                        arr[j] =arr[j+1];
-                    }
 
 
-                        n--;
+int main () {
 
-                       
+
+
+
+
+
+
+int arr [] {1,2,3,4,4,5,6,7,7,8};
+
+size_t n = sizeof(arr) / sizeof(arr[0]);
+
+
+for (int i = 0; i <n; i++){
+
+    std::cout << "  " << arr[i];
+
+
+}
+
+std::cout << "\n searching for duplicates ...................................................." << std::endl;;
+
+
+
+for (int i = 0; i < n; i++) 
+
+            for (int j = i+1; j < n; ) {
                     
+                    if (arr[i] == arr[j]) {
+                                                    
+                            for (int k = j;k < n-1;k++){
+                                arr[k] = arr[k+1];
+                                
+                            }
+                                
 
+                   
+                n--;
+                    }
+     else{ j++;}
             }
-           else {
-               k++;
-           }
-        }
- 
 
-    std::cout << "\n size of array is " << n << std::endl;
 
-    for (int i = 0; i < n; i++)
-    {
 
-        std::cout << arr[i] << "  ";
-    }
+for (int i = 0; i <n; i++){
 
-    return 0;
+    std::cout << "  " << arr[i];
+
+
+}
+return 0;
+
 }
