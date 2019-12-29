017ed1de32d843ac97111e212d4f4625936ef127
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-29 16:11:06 +0100

    interview examples

--- testSpace/main.cpp
+++ testSpace/main.cpp
@@ -1,58 +1,93 @@
 #include <iostream>
 
+void swap(int *a, int *b);
+
+void print (int arr[], int n);
+
+void quickSort(int arr[],int low, int high);
+
+int partition (int arr[], int low, int high);
+
+
 
 
 int main () {
 
+std::cout << "  \n \n \n QuickSOrt algorithm \n \n \n";
+
+int arr[] {1,4,3,2,5,4,33,2,4343,11,22,33,55,33,88,55,77,66,44,99,78,665,342,2,765,777,43,77};
+
+size_t n = sizeof(arr) / sizeof (arr[0]);
+
+int low = 0;
+
+int high= (n-1);
+
+print (arr,n);
 
 
+std::cout << " \n \n now sorting your array ............................................................................ \n \n" << std::endl;
 
+quickSort(arr,0,high);
 
+print (arr,n);
 
-
-int arr [] {1,2,3,4,4,5,6,7,7,8};
-
-size_t n = sizeof(arr) / sizeof(arr[0]);
-
-
-for (int i = 0; i <n; i++){
-
-    std::cout << "  " << arr[i];
-
-
+    return 0;
 }
 
-std::cout << "\n searching for duplicates ...................................................." << std::endl;;
+
+
+void swap(int *a, int *b) {
+
+
+int temp = *a;
+*a = *b;
+*b = temp;
+}
 
 
 
-for (int i = 0; i < n; i++) {
+void print (int arr[], int n) {
 
-            for (int j = i+1; j < n; ) {
-                    
-                    if (arr[i] == arr[j]) {
-                                                    
-                            for (int k = j;k < n-1;k++){
-                                arr[k] = arr[k+1];
-                                
-                            }
-                                
+for (int i = 0; i < n; i ++) {
 
-                   
-                n--;
-                    }
-     else{ j++;}
+    std::cout << " " << arr[i];
+
+}
+}
+
+
+void quickSort(int arr[], int low, int high) {
+
+
+  if ( low < high) {
+
+        int pi = partition(arr,low,high);
+
+      quickSort(arr, low, pi-1);
+        quickSort(arr,pi +1, high);
+      
+
+
+    }
+}
+
+
+int partition (int arr[], int low, int high) {
+
+        int pIndex = (low -1);
+        int pivot = arr[high];
+
+        for (int i = low; i < high; i ++) {
+
+                if (arr[i] < arr[high]) {
+                    pIndex++;
+                    swap(&arr[pIndex],&arr[i]);
+                }
+         
+         
             
-            }
-
-
-}
-for (int i = 0; i <n; i++){
-
-    std::cout << "  " << arr[i];
-
-
-}
-return 0;
-
+        }
+ swap(&arr[pIndex+1], &arr[high]);
+return (pIndex+1);
 }
