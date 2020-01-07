e39b106a9f8bddc17ac8cf20dc627ca948f835a1
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-07 16:11:29 +0100

    psuedo

--- findingPair/main.cpp
+++ findingPair/main.cpp
@@ -4,6 +4,36 @@
 
 int main () {
 
+int arr[] {1,2,3,4,5,6};
+
+int target {9};
+
+size_t n = sizeof(arr) / sizeof(arr[0]);
+
+int low{};
+
+int high{};
+
+// while loop to start loop
+
+
+
+
+// if low + high do not = target do ....
+
+
+
+// if sum is to small , low + 1
+
+// i sum to big high -1
+
+
+
+// if nothing cout npot there
+
+
+
+
 
 
 
