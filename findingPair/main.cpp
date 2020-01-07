c0773b2eb79891d8c0c369d6270722cebb7c4939
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-07 16:22:44 +0100

    complete o(log(n))

--- findingPair/main.cpp
+++ findingPair/main.cpp
@@ -1,53 +1,43 @@
 #include <iostream>
 
+int main()
+{
+
+    int arr[]{1, 2, 3, 4, 5, 6};
+
+    int target{9};
+
+    size_t n = sizeof(arr) / sizeof(arr[0]);
+
+    int low{};
+
+    int high = n - 1;
+
+    // while loop to start loop
+
+    low = 0;
+
+    while (low < high)
+    {
+
+        if (arr[low] + arr[high] == target)
+        {
+            std::cout << " target met in array " << arr[high] << "  and  " << arr[low] << std::endl;
+           break;
+        }
+
+        else if (arr[low] + arr[high] < target)
+        {
+
+            low++;
+        }
+
+        else{
 
 
-int main () {
+            high++;
+        }
+    }
 
-int arr[] {1,2,3,4,5,6};
-
-int target {9};
-
-size_t n = sizeof(arr) / sizeof(arr[0]);
-
-int low{};
-
-int high{};
-
-// while loop to start loop
-
-while (low < high) {
-
-        if (arr[low] + arr[high] == target) std::cout << " target met in array ." << arr[high] << "  and  " << arr[low] << std::endl;    
-    
-
-            else if (arr[low] + arr[high] < target) {
-                low++;
-            }
-
-            else high++;
-
-
-}
-
-
-
-// if sum is to small , low + 1
-
-// i sum to big high -1
-
-
-
-// if nothing cout npot there
-
-
-
-
-
-
-
-
-std::cout << " test find pair ";
-
-    return 0 ;
+    return 0;
 }
