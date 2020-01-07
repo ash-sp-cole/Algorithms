87f6e4ab30f813fcf62d4b419bc685477ba2824f
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-07 12:30:13 +0100

    oop

--- objectOP/main.cpp
+++ objectOP/main.cpp
@@ -5,61 +5,20 @@
 
 using namespace std;
 
+class Account {
 
+private:
+int balance;
+public: 
+void setBalance(int balance);
 
-class Player {
-
-// attributes 
-public :
-string name{};
-int health {};
-int xp {};
-
-
-// methods
-
-
-void talk (string input) {
-
-    cout << name << "   says  " << input << endl;;  
-}
-
-bool isDead (int health) {
-
-if (health <=0) {
-    cout << "is dead" << endl;
-    return true;
-
-}
-
-else {
-
-    cout << " is alive " << endl;
-    return false;
-
-}
-
-}
 
 
 };
 
 
+
 int main () {
-Player frank;
-Player james;
-frank.name = "ash";
-frank.health = 100;
-james.name = "james";
-
-frank.talk("hello world");
-frank.isDead(0);
-
-Player *enemy = new Player;
-
-(*enemy).name = "enemy";
-
-enemy->talk("hi enemny");
 
 return 0;
 
