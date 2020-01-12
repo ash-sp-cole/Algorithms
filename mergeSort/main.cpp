a362088f0259d9e77507f622510074d16b720f50
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-12 14:11:45 +0100

    mergesort

---
+++ mergeSort/main.cpp
@@ -0,0 +1,28 @@
+#include < iostream>
+
+
+
+int main () {
+
+int arr[] {7,5,3,6,1,67,3,88,55,33,22,11,8};
+
+size_t n = sizeof(arr)/ sizeof(arr[0]);
+
+
+
+std::cout << "test merge";
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
