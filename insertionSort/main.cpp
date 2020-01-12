c4d6919c6549af77047705b380e67f840cca8a93
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-12 14:04:40 +0100

    update

--- insertionSort/main.cpp
+++ insertionSort/main.cpp
@@ -11,9 +11,21 @@ int arr[] { 1,4,3,2,5,7,4,2,3,1};
 
 size_t n = sizeof(arr) /sizeof(arr[0]);
 
+
+int key{};
 for (int i = 0; i <n; i++) {
 
+key =arr[i];
 
+int j = (i -1);
+
+    while (j <= 0 && arr[j]> key) {
+
+        arr[j+1] = arr[j];
+        j = j-1;
+
+    }
+        arr[j +1 ] = key;
 
 
 
