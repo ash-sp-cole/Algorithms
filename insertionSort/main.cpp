5a2a1f74d89c3becfb74940f8b120a0bb7476c73
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-12 13:57:37 +0100

    insertion sort

---
+++ insertionSort/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main () {
+
+int arr[] { 1,4,3,2,5,7,4,2,3,1};
+
+size_t n = sizeof(arr) /sizeof(arr[0]);
+
+for (int i = 0; i <n; i++) {
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+for (int k =0; k < n; k++){
+
+cout << " " << arr[k];
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
