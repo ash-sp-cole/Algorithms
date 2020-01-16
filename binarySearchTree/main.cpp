11ccca78911de75ea590f2d6cdf73c68e2244327
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-16 14:19:44 +0100

    node structure

--- binarySearchTree/main.cpp
+++ binarySearchTree/main.cpp
@@ -1,45 +1,39 @@
 #include <iostream>
 
 
+/// define bst nide tree
+struct BSTNode 
+{
+    int data{};
 
-using namespace std;
-struct Node {
-
-int data{};
-
-Node* next{};
-
+    BSTNode* left{};
+    BSTNode* right{};
 };
+
+
+// creat new nodes in the HEAP
+
+
+BSTNode* getNewNode (int data) {
+
+BSTNode* newNode = new BSTNode();
+newNode->data = data;
+newNode ->left = newNode->right = NULL;
+return newNode;
+}
 
 
 
 int main () {
 
 
-Node* head = NULL;
-Node* second = NULL;
-Node* third = NULL;
-
-head = new Node();
-second = new Node();
-third = new Node();
 
 
 
-head->data = 1;
-head->next = second;
-
-second->data = 2;
-second->next =third;
-
-third->data =7;
-third->next = NULL;
 
 
-while (head != NULL){
-    cout << "  node value " << head->data;
-    head = head->next;
-}
+
+
 
 
     return 0;
