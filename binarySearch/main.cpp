22feed0aaede97ea7030d81eb9efe6dce28e9630
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-20 14:49:55 +0100

    complete

--- binarySearch/main.cpp
+++ binarySearch/main.cpp
@@ -13,16 +13,27 @@ int high = (n -1 );
 int mid {};
 std::cout << " please enter tagert value \n \n" << std::endl;
 std::cin >> target;
-std::cout << " \ n" << std::endl;
+std::cout << " \n" << std::endl;
 
 while (low <= high) {
 
 mid = (low + high) / 2;
 
+        if ( arr[mid] == target) {
 
+            std::cout << " target value is found at " << mid << " value is  " << arr[mid] << std::endl;
+            break;
+        }
 
+    else if (arr[mid] < target) {
 
+        low = (mid + 1);
+    }
 
+    else if ( arr[mid] > target) {
+high = (mid -1);
+
+    }
 
 
 
